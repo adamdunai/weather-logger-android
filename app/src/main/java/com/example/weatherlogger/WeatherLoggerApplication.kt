6d63bc9e503fb4f7fe1b36e12dbf5735f01915c1
Adam Dunai <dunai.adam95@gmail.com>
2022-02-12 11:16:34 +0100

    Set up logging

--- app/src/main/java/com/example/weatherlogger/WeatherLoggerApplication.kt
+++ app/src/main/java/com/example/weatherlogger/WeatherLoggerApplication.kt
@@ -1,5 +1,13 @@
 package com.example.weatherlogger
 
 import android.app.Application
+import logcat.AndroidLogcatLogger
+import logcat.LogPriority
 
-class WeatherLoggerApplication : Application()
+class WeatherLoggerApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        AndroidLogcatLogger.installOnDebuggableApp(this, LogPriority.VERBOSE)
+    }
+}
