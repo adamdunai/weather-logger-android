7d67c193c0c7b139a4a2bc52aa689d3a533a0700
Adam Dunai <dunai.adam95@gmail.com>
2022-02-12 11:25:20 +0100

    Enable view binding

--- app/src/main/java/com/example/weatherlogger/main/MainActivity.kt
+++ app/src/main/java/com/example/weatherlogger/main/MainActivity.kt
@@ -2,13 +2,18 @@ package com.example.weatherlogger.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.weatherlogger.R
+import com.example.weatherlogger.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        activityBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityBinding.root)
     }
 }
